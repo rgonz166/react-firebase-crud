1b9b6b5a5a1acf3e604bde66aded4b72d6dfd0d9
Raul Gonzalez <rgonz166@gmail.com>
2022-02-12 03:41:24 -0800

    Add Firebase files

---
+++ src/firebase.tsx
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp,  } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC7pOu6qCRghbMKnavHU3wN4J8IRd7KXVc",
+  authDomain: "react-firebase-crud-6fac3.firebaseapp.com",
+  projectId: "react-firebase-crud-6fac3",
+  storageBucket: "react-firebase-crud-6fac3.appspot.com",
+  messagingSenderId: "859022452129",
+  appId: "1:859022452129:web:79d2568e63ec40f5f417e0"
+};
+
+// Initialize Firebase
+const fb = initializeApp(firebaseConfig);
+export default fb;
